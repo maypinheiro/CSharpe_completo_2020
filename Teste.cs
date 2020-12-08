1230656362b8a937d0af14cf69d96809f8b7260f
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2020-12-08 14:28:35 -0300

    teste
    Teste

---
+++ Teste.cs
@@ -0,0 +1,19 @@
+using System.Globalization;
+using System.Security.AccessControl;
+using System;
+
+namespace curso
+{
+    class Teste
+    {
+        static void Main(string[]args)
+        {
+            double a;
+            float b;
+
+            a= 5.1
+            b=a;
+            System.Console.WriteLine(a);
+        }
+    }
+}
